d89ee9e514969db775a1f87dccacd0a8a1be8b30
EC2 Default User <ec2-user@ip-172-31-85-33.ec2.internal>
2023-06-21 11:05:05 +0000

    maven project

---
+++ maven-devops2/src/main/java/devops16/App.java
@@ -0,0 +1,13 @@
+package devops16;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
